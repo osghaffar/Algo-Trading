0b66e1f95feb8e824227be058138b284437d6c7c
Omar Ghaffar <35759477+osghaffar@users.noreply.github.com>
2018-12-23 05:17:59 -0600

    Update and rename ordertotal.cpp to ordercounter.cpp

---
+++ Ichimoku Trading Algorithm/ordercounter.cpp
@@ -0,0 +1,21 @@
+//This function keeps track of the number of orders that have been placed
+//by using the orders "magic number", which is an arbitrary value that
+//you assign which allows it to be identified. It's best to have a diff
+//magic number for buy orders and sell orders.
+
+double OrderCounter(int magic_number)
+{    
+    //we pass in the order's magic number and then use a basic for loop to
+    //keep track of the number of trades.
+    
+    double totalOrders = 0;
+    
+    for(int count = 0; count < OrdersTotal(); count++){
+        OrderSelect(count, SELECT_BY_POS, MODE_TRADES);
+        if(OrderMagicNumber() == magic_number){
+            totalOrders += 1;
+        }
+    }
+    
+    return(totalOrders);
+}
