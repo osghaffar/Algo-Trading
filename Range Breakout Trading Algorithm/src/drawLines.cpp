4e8d5bde537cbc4a29e6b9cb7b96cb18e616d4eb
Omar Ghaffar <35759477+osghaffar@users.noreply.github.com>
2020-04-29 21:50:17 -0500

    Update drawLines.cpp

--- Range Breakout Trading Algorithm/src/drawLines.cpp
+++ Range Breakout Trading Algorithm/src/drawLines.cpp
@@ -1,10 +1,17 @@
+/*
+Draws lines at the top and bottom of the consolidation period, serving as a buy/sell signal threshold.
+Length and position can all be adjusted, using the following function from the documentation:
+
+ObjectCreate(name, type, time_period, x_leftmost, y_leftmost, x_rightmost, y_rightmost)
+
+*/
 void drawLines(double periodHighest, double periodLowest){
    
    //draw lines, top and bottom
    ObjectCreate("hLine",OBJ_TREND,0,Time[11],periodHighest,Time[0],periodHighest);
    ObjectCreate("lLine",OBJ_TREND,0,Time[11],periodLowest,Time[0],periodLowest);
      
-   //dont make them extend all the way across the screen, i.e not rays
+   //don't make them extend all the way across the screen, i.e not rays
    ObjectSet("hLine", OBJPROP_RAY,false);
    ObjectSet("lLine", OBJPROP_RAY,false);
 }
