4f1847cf78ecc9431b6c934a0b9787b62a4a398b
Omar Ghaffar <35759477+osghaffar@users.noreply.github.com>
2020-04-29 21:52:41 -0500

    Update drawShape.cpp

--- Range Breakout Trading Algorithm/src/drawShape.cpp
+++ Range Breakout Trading Algorithm/src/drawShape.cpp
@@ -1,3 +1,14 @@
+/*
+Draws a rectangle around the consolidation period, serving as a visual aid to the buy/sell signal threshold.
+Length and position can all be adjusted, using the following function from the documentation:
+
+ObjectCreate(name, type, time_period, x_leftmost, y_leftmost, x_rightmost, y_rightmost)
+
+Color can also be adjusted:
+
+ObjectSet(name, attribute, color_name)
+
+*/
 void drawShape(double periodHighest, double periodLowest){
    
    //draw rectangle (name, object, currentchart, left edge, top edge, right edge, bottom edge)
